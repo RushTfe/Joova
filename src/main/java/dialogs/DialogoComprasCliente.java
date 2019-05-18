8b36029a7d7ef09b8477acc0e3aa76754d405d71
pgalindo <44260567+RushTfe@users.noreply.github.com>
2019-05-18 20:04:51 +0200

    Generado el dialogo con los productos de cada venta en el reporte

---
+++ src/main/java/dialogs/DialogoComprasCliente.java
@@ -0,0 +1,52 @@
+package dialogs;
+
+import app.JoovaApp;
+import javafx.beans.property.ListProperty;
+import javafx.geometry.Insets;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.NuevoProductoModel;
+
+import javax.swing.text.html.ImageView;
+
+public class DialogoComprasCliente extends Dialog {
+    private TableView<NuevoProductoModel> tablaProductos;
+    private TableColumn<NuevoProductoModel, ImageView> imagenColumn;
+    private TableColumn<NuevoProductoModel, String> nombreArticuloColumn;
+    private TableColumn<NuevoProductoModel, String> descripcionArticuloColumn;
+    private TableColumn<NuevoProductoModel, String> tipoArticuloColumn;
+
+    public DialogoComprasCliente(ListProperty<NuevoProductoModel> listaProductos) {
+        tablaProductos = new TableView<>();
+        imagenColumn = new TableColumn<>();
+        nombreArticuloColumn = new TableColumn<>("Nombre");
+        descripcionArticuloColumn = new TableColumn<>("Descripción");
+        tipoArticuloColumn = new TableColumn<>("Tipo");
+
+        tablaProductos.getColumns().add(0, imagenColumn);
+        tablaProductos.getColumns().add(1, nombreArticuloColumn);
+        tablaProductos.getColumns().add(2, descripcionArticuloColumn);
+        tablaProductos.getColumns().add(3, tipoArticuloColumn);
+
+        tablaProductos.itemsProperty().bind(listaProductos);
+        tablaProductos.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        tablaProductos.setPrefWidth(900);
+        tablaProductos.setPrefHeight(600);
+
+        nombreArticuloColumn.setCellValueFactory(v -> v.getValue().nombreProductoProperty());
+        tipoArticuloColumn.setCellValueFactory(v -> v.getValue().tipoProductoProperty());
+        descripcionArticuloColumn.setCellValueFactory(v -> v.getValue().descripcionProductoProperty());
+        imagenColumn.setCellValueFactory(new PropertyValueFactory<NuevoProductoModel, ImageView>("imagen"));
+
+        tablaProductos.setPadding(new Insets(5));
+
+        initOwner(JoovaApp.getPrimaryStage());
+
+        getDialogPane().getButtonTypes().addAll(ButtonType.OK);
+
+        getDialogPane().setContent(tablaProductos);
+    }
+}
